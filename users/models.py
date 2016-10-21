40b5b0945c8f31c0edb028166ef6ff5d7a44626a
Guillermo Colmenero <guillermocolmenero@gmail.com>
2016-10-21 12:15:24 -0300

    Adding post save signal to UserProfile.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.conf import settings
 from django.db.models import Model, TextField, OneToOneField, CASCADE
+from django.db.models.signals import post_save
 
 
 class UserProfile(Model):
@@ -13,3 +14,11 @@ class UserProfile(Model):
     class Meta:
         verbose_name = 'user profile'
         verbose_name_plural = 'user profiles'
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(create_user_profile, sender=settings.AUTH_USER_MODEL)
