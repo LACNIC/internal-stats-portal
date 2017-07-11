9241d9cca0d08807e5fa42cfc81ad74eb627b35b
Agustín <aguformoso@gmail.com>
2017-07-11 14:39:22 -0300

    slash append

--- opendata/urls.py
+++ opendata/urls.py
@@ -6,7 +6,7 @@ from views import home, categoria, busqueda, dato
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-url(r'^categoria$', categoria, name='categoria'),
-url(r'^busqueda', busqueda, name='busqueda'),
-url(r'^dato', dato, name='dato')
+url(r'^categoria/$', categoria, name='categoria'),
+url(r'^busqueda/$', busqueda, name='busqueda'),
+url(r'^dato/$', dato, name='dato')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
