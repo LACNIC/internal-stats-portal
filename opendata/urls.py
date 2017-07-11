4e769b3dd0e44591ec0bcb68fe7905d426cb13a3
Agustín <aguformoso@gmail.com>
2017-07-11 14:30:06 -0300

    Dynamic pages

--- opendata/urls.py
+++ opendata/urls.py
@@ -5,8 +5,11 @@ from django.conf.urls.static import static
 from views import home, categoria, busqueda, dato
 
 urlpatterns = [
-    url(r'^$', home, name='home'),
-url(r'^categoria$', categoria, name='categoria'),
-url(r'^busqueda', busqueda, name='busqueda'),
-url(r'^dato', dato, name='dato')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  url(r'^$', home, name='home'),
+
+                  url(r'^categoria/$', categoria, name='categoria'),  # no cambiar el orden, no sacar el $
+                  url(r'^categoria/(?P<tag>\w+)?', categoria, name='categoria'),
+
+                  url(r'^busqueda', busqueda, name='busqueda'),
+                  url(r'^dato', dato, name='dato')
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- opendata/views.py
+++ opendata/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from core.models import *
 
 
 # Create your views here.
@@ -6,11 +7,23 @@ from django.shortcuts import render
 def home(request):
     return render(request, "opendata/home.html")
 
-def categoria(request):
-    return render(request, "opendata/categoria.html")
+
+def categoria(request, tag=''):
+    publicaciones = Publication.objects.filter(tags__name=tag)
+
+    return render(
+        request,
+        "opendata/categoria.html",
+        context={
+            'categoria': tag,
+            'pubs': publicaciones
+        }
+    )
+
 
 def busqueda(request):
     return render(request, "opendata/busqueda.html")
+
 
 def dato(request):
     return render(request, "opendata/dato.html")
