e029a5cebc3341af51b0cbcb2caf74d6c630168a
Agustín <aguformoso@gmail.com>
2017-08-30 16:25:06 -0300

    Redirect models

--- opendata/models.py
+++ opendata/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
-
+from datetime import datetime
 from django.db import models
+from core.models import Publication
 
-# Create your models here.
+
+class Redirect(models.Model):
+    """
+        class to persist all redirects served
+    """
+    redirect_from = models.URLField()
+    redirect_to = models.URLField()
+    date = models.DateTimeField(default=datetime.now)
+
+    publication = models.ForeignKey(Publication, default=1)
