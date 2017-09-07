971000d3376341f3e7c052397e0f7ae19bef27b2
Agustín <aguformoso@gmail.com>
2017-09-07 12:17:03 -0300

    Visits count

--- opendata/models.py
+++ opendata/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from datetime import datetime
 from django.db import models
-from core.models import Publication
+from core.models import Publication, Data
 
 
 class Redirect(models.Model):
@@ -13,3 +13,12 @@ class Redirect(models.Model):
     date = models.DateTimeField(default=datetime.now)
 
     publication = models.ForeignKey(Publication, default=1)
+
+
+class Visit(models.Model):
+    """
+        class to persist all visits to Data pages
+    """
+    url = models.URLField()
+    date = models.DateTimeField(default=datetime.now)
+    publication = models.ForeignKey(Publication)
